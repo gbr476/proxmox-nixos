ac3ddd4e81be0109e4eae058e9aaab0459f2a600
Camille M. (genesis) <camillemondon@free.fr>
2024-08-16 16:00:16 +0200

    feat: add lvm disk to linstor test

--- tests/linstor.nix
+++ tests/linstor.nix
@@ -8,7 +8,10 @@
         enable = true;
         linstor.enable = true;
       };
-      virtualisation.memorySize = 2048;
+      virtualisation = {
+        emptyDiskImages = [ 4096 ];
+        memorySize = 2048;
+      };
     };
     pve2 = {
       services.proxmox-ve = {
@@ -29,6 +32,12 @@
     pve1.wait_for_unit("linstor-controller.service")
     pve2.wait_for_unit("multi-user.target")
 
+    pve1.succeed("vgcreate linstor_vg /dev/vdb")
+    pve1.succeed("lvcreate -l 80%FREE -T linstor_vg/thinpool")
+    pve1.succeed("linstor storage-pool create lvmthin pve1 pve-storage linstor_vg/thinpool")
+    pve1.succeed("linstor resource-group create pve-rg --storage-pool=pve-storage --place-count=1")
+    pve1.succeed("linstor volume-group create pve-rg")
+
     storageCfg = """
     drbd: linstor_storage
         content images, rootdir
@@ -39,6 +48,8 @@
     pve1.succeed("cat /etc/pve/storage.cfg")
     assert "9.2" in pve1.succeed("modinfo drbd")
     assert "drbd" in pve1.succeed("cat /proc/modules")
+
+    pve1.succeed("systemctl restart pve-cluster pvedaemon pvestatd pveproxy")
 
     pve1.succeed("linstor node create pve1 192.168.0.1")
   '';
